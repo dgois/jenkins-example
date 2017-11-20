a3386627367f9d96022fb7cb5c09b91990f29f9b
Denis Gois <denisdwt@gmail.com>
2017-11-20 17:07:29 -0500

    First Commit

---
+++ test/ArithmeticTest.java
@@ -0,0 +1,18 @@
+
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class ArithmeticTest {
+	
+	@Test
+	public void shouldSumTwoNumbers() throws Exception {
+		Arithmetic arithmetic = new Arithmetic(1, 2);
+		
+		long actual = arithmetic.calculate("+");
+		
+		assertEquals(3, actual);
+	}	
+
+}
